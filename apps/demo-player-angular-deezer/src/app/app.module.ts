ab98d321d286cf914932d5b3597a374bb48beccf
Redelta <lucascampistron95@gmail.com>
2021-05-20 14:56:59 +0200

    feat(app): add deezer demo for angular

---
+++ apps/demo-player-angular-deezer/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { PlayerComponent } from './player/player.component';
+import { HomeComponent } from './home/home.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { PlaylistCreatorComponent } from './playlist-creator/playlist-creator.component';
+import { PlaylistItemComponent } from './playlist-item/playlist-item.component';
+
+@NgModule({
+	declarations: [AppComponent, PlayerComponent, HomeComponent, PlaylistComponent, PlaylistCreatorComponent, PlaylistItemComponent],
+	imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'playlist', component: PlaylistCreatorComponent },
+      { path: 'playlist/:id', component: PlaylistComponent }
+    ], { initialNavigation: 'enabled' }),
+    MatIconModule,
+    FormsModule,
+  ],
+	providers: [],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}
